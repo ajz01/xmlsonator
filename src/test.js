cc3ee45759f232220392ea1d212d4645f690d230
andrew <azdenek@yahoo.com>
2015-10-31 00:53:21 -0400

    fix conversion algorithm

--- src/test.js
+++ src/test.js
@@ -1,15 +1,25 @@
 var xsr = require('./xmlsonator.js');
 
-var xml = "<xsr:Xmlsonator xmlns:xsr='http://xmlsonator.org/schema'>" +
+/*var xml = "<xsr:Xmlsonator xmlns:xsr='http://xmlsonator.org/schema'>" +
       "<xsr:Sample name='test1' emptyAttribute='' xsr:ns_id='xmlson'>" +
       "</xsr:Sample>" +
-      "</xsr:Xmlsonator>";
+      "</xsr:Xmlsonator>";*/
+
+var xml = "<menu id='file' value='File'> \
+  <popup> \
+    <menuitem value='New' onclick='CreateNewDoc()' /> \
+    <menuitem value='Open' onclick='OpenDoc()' /> \
+    <menuitem value='Close' onclick='CloseDoc()' /> \
+  </popup> \
+</menu>";
+
+var buffer = Buffer(xml, 'utf-8');
 
 console.time('n-parses');
 
-for(var i = 0; i < 10000; i++)
-  var jobj = xsr.parseXML(Buffer(xml, 'utf-8'));
+//for(var i = 0; i < 10000; i++)
+  var jobj = xsr.parseXML(buffer);
 
 console.timeEnd('n-parses');
 
-console.log(jobj);
+console.log(jobj.popup);
